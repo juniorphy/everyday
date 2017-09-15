eaacf059ebc65c3a5d8abc43c33608d48d426009
junior <francisco.vasconcelos@funceme.br>
2017-09-15 14:20:38 -0300

    add script

--- thiessen-etp/comp_thiessen_etp.py
+++ thiessen-etp/comp_thiessen_etp.py
@@ -1,10 +1,10 @@
 import numpy as np
 from PyFuncemeClimateTools import Thiessen
-from hidropy.utils import write_thiessen
+from hidropy.utils.write_thiessen import write_thiessen
 
 fin = '/home/junior/Documents/data/pet_daily_inmet/pet_daily_inmet_19610101_20161231.asc'
 
-shape = '/home/junior/Documents/programs/shapes_funceme/reservatorio/reservatorio-210-gameleira-CE.asc'
+shape = '/home/junior/Documents/programs/shapes_funceme/bacia_hidro/bacia_hidro-31-litoral-CE.asc'
 
 mmm = np.loadtxt(fin)
 
@@ -15,9 +15,7 @@ lon = mmm[:,1]
 
 thi = Thiessen.thiessen(raw,lat,lon,shape,pf=3, num_proc=8)
 
-thi = np.squeeze(thi)
+#thi = np.squeeze(thi)
 
-write_thiessen(thi,19610101,20161231, daily, pet, inmet)
-
-
+write_thiessen(thi, '19610101', '20161231', 'daily', 'pet', 'inmet', 'obs','litoral', '')
 
