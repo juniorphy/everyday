5793f8030cdc240f4409bdad9d8f198744655c1d
junior <francisco.vasconcelos@funceme.br>
2017-09-15 13:47:52 -0300

    initial commit

---
+++ thiessen-etp/comp_thiessen_etp.py
@@ -0,0 +1,23 @@
+import numpy as np
+from PyFuncemeClimateTools import Thiessen
+from hidropy.utils import write_thiessen
+
+fin = '/home/junior/Documents/data/pet_daily_inmet/pet_daily_inmet_19610101_20161231.asc'
+
+shape = '/home/junior/Documents/programs/shapes_funceme/reservatorio/reservatorio-210-gameleira-CE.asc'
+
+mmm = np.loadtxt(fin)
+
+raw = mmm[:, 3:]
+
+lat = mmm[:,2]
+lon = mmm[:,1]
+
+thi = Thiessen.thiessen(raw,lat,lon,shape,pf=3, num_proc=8)
+
+thi = np.squeeze(thi)
+
+write_thiessen(thi,19610101,20161231, daily, pet, inmet)
+
+
+
